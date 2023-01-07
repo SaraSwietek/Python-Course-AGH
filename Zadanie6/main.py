930a409e7be7526a29623af8ab8c2cd9ad012aaa
SaraSwietek <saraswietek@student.agh.edu.pl>
2023-01-07 18:23:00 +0100

    final final version

--- Zadanie6/main.py
+++ Zadanie6/main.py
@@ -2,7 +2,6 @@ import pandas as pd
 from datetime import timedelta, datetime
 import matplotlib.pyplot as plt
 
-
 if __name__ == '__main__':
     df1 = pd.read_csv("Plant_1_Generation_Data.csv", parse_dates=['DATE_TIME'])  # header=0 default
     df2 = pd.read_csv("Plant_2_Generation_Data.csv", parse_dates=['DATE_TIME'])
@@ -10,35 +9,48 @@ if __name__ == '__main__':
 
     df = df.dropna().reset_index(drop=True)  # usuwanie NaN i Null + reset indeksow
 
-    # plot AC_POWER dla wybranego generatora w wybranym tygodniu
+    # wybor tygodnia + przykladowego generatora
 
     start_date = datetime(2020, 5, 15)
     end_date = start_date + timedelta(weeks=1)
 
     df_week = df[(df['DATE_TIME'] >= start_date) & (df['DATE_TIME'] <= end_date)]  # df dla 1 tygodnia
-    df_week = df_week.loc[df['PLANT_ID'] == 4135001]  # dla generatora 4135001
+    df_week_single_gen = df_week.loc[df['SOURCE_KEY'] == 'oZZkBaNadn6DNKz']  # dla generatora oZZkBaNadn6DNKz
 
-    df_plot = df_week.plot(x='DATE_TIME', y='AC_POWER')
+    # grupowanie po generatorach (source key) i czasie (date time)
+    # obliczanie sredniej dla kazdego generatora w danym momencie
+    df_week_mean_all_gen = df_week.groupby("DATE_TIME")['AC_POWER'].mean()
 
-    # obliczanie sredniej dla obu generatorow
-
-    df_gen1 = df.loc[df['PLANT_ID'] == 4135001]
-    df_gen2 = df.loc[df['PLANT_ID'] == 4136001]
-
-    gen1_mean = df_gen1.AC_POWER.mean()
-    gen2_mean = df_gen2.AC_POWER.mean()
-
-    df_plot.hlines(y=gen1_mean, xmin=start_date, xmax=end_date, color='purple', label='Plant 1')
-    df_plot.hlines(y=gen2_mean, xmin=start_date, xmax=end_date, color='red', label='Plant 2')
-
-    df_plot.legend()
+    ax = df_week_mean_all_gen.plot(x='DATE_TIME', y='AC_POWER', label="Mean value all")
+    df_week_single_gen.plot(ax=ax, x='DATE_TIME', y='AC_POWER', color="orange", label="Sample")
+    plt.title("Comparison (Sample vs. Sample generator)")
+    plt.ylabel("AC-Power")
+    plt.xlabel("Date")
+    plt.legend()
     plt.show()
 
-    # przypadki, kiedy AC_POWER któregoś z generatorów było na poziomie < 80% średniej
+    # Znajdź przypadki, kiedy AC_POWER któregoś z generatorów było na poziomie < 80% średniej.
+    # Których generatorów najczęściej to dotyczy?
 
-    df_compare_to_mean1 = df_gen1[(df_gen1['AC_POWER'] <= gen1_mean*0.8)]
-    df_compare_to_mean2 = df_gen2[(df_gen2['AC_POWER'] <= gen2_mean*0.8)]
+    df_mean_all_gen = pd.DataFrame(df.groupby("DATE_TIME")['AC_POWER'].mean())
+    df_mean_all_gen['DATE_TIME'] = df_mean_all_gen.index
+    df_mean_all_gen['AC_POWER_80%'] = 0.8*df_mean_all_gen['AC_POWER']
+    df_mean_all_gen.reset_index(drop=True, inplace=True)
+    df_mean_all_gen.drop(['AC_POWER'], axis=1, inplace=True)
+    df = df.merge(df_mean_all_gen, how = 'left', on='DATE_TIME')
+    df_compare = df[(df['AC_POWER'] < df['AC_POWER_80%'])]
 
-    print("--- AC_POWER generatora 2 jest częściej na poziomie < 80% średniej ---")
-    print("Plant 1: AC_POWER < 80% średniej: ", round(df_compare_to_mean1.size / df_gen1.size * 100, 2), "% czasu")
-    print("Plant 2: AC_POWER < 80% średniej: ", round(df_compare_to_mean2.size / df_gen2.size * 100, 2), "% czasu")
+    print("Generatory które najczęściej wytwarzają energię poniżej 80% średniej:")
+    print(df_compare.groupby("SOURCE_KEY").size().to_frame('size').sort_values(["size"], ascending=False))
+
+    # Porównanie najsłabszego generatora ze średnią
+
+    df_week_weak_gen = df_week.loc[df['SOURCE_KEY'] == 'Quc1TzYxW2pYoWX']  # dla generatora Quc1TzYxW2pYoWX
+    ax = df_week_mean_all_gen.plot(x='DATE_TIME', y='AC_POWER', label="Mean value all")
+    df_week_weak_gen.plot(ax=ax, x='DATE_TIME', y='AC_POWER', color="orange", label="Weak sample")
+
+    plt.title("Comparison (Sample vs. Weakest Generator)")
+    plt.ylabel("AC-Power")
+    plt.xlabel("Date")
+    plt.legend()
+    plt.show()
